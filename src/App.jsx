c2a11c0363604d976da9e6c655572b645a601c09
abnsol <abenisolo51196@gmail.com>
2025-03-12 16:09:03 +0300

    compose components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CardContainer from "./components/CardContainer"
+import { useEffect } from "react"
+import { useState, useRef } from "react";
+import GameOver from "./components/GameOver";
+import GameBoard from "./components/GameBoard";
+import styles from './App.module.css'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [pokemonData,setPokemonData] = useState([]);
+  let [playStatus,setPlayStatus] = useState(true);
+  const clickedCards = useRef(new Set());
+  let highScore = localStorage.getItem("highScore")
+  highScore = isNaN(highScore) ? 0 : highScore
+  // console.log(isNaN(highScore))
+  // console.log("highScore",highScore)
+
+  useEffect(() => {
+    (async() => {
+      // fetch the pokemon 
+      const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50&offset=0')
+                .then((response) => response.json())
+                .then((data) => data.results)
+      
+      // filter the url
+      const pokemon_url = await pokemon.map((poke) => poke.url);
+
+      // // fetch each pokemon using the url and promise all
+      const promisedPokemonList = await pokemon_url.map((url) => 
+        fetch(url).then((response) => response.json())
+      )
+      const pokemonList = await Promise.all(promisedPokemonList)
+
+      // filter the name the id the pic
+      const pokemons = pokemonList.map((pokemon) => {
+        return {
+          name: pokemon.name,
+          pic: pokemon.sprites.front_default,
+          id: pokemon.id
+        }
+      })
+      setPokemonData(pokemons)
+    })();
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    playStatus ? (
+      <div className={styles.app}>
+        <GameBoard clickedCards={clickedCards} highScore={highScore}/>
+        <CardContainer pokemonData={pokemonData} setPokemonData={setPokemonData} setPlayStatus={setPlayStatus} clickedCards={clickedCards.current} highScore={highScore}/> 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )
+      : <GameOver setPlayStatus={setPlayStatus}/>
   )
 }
 
